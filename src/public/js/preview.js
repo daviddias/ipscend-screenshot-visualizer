6d22ba2b2df5da4e27072c9dc01cf5f639cf2f29
David Dias <daviddias.p@gmail.com>
2015-10-10 08:54:39 -0700

    add gifshot

--- src/public/js/preview.js
+++ src/public/js/preview.js
@@ -1,6 +1,8 @@
 // just for testing
 var hash = window.document.location.hash || '#QmdqoXTW5WGEMiU1QiUnhBMHQpiVnQ8sKSUrN3pb6k3PxF'
 
+var screenshotURLs = []
+
 if (hash !== '') {
   var ipfs = window.ipfsAPI('localhost', '5001')
   hash = hash.slice(1)
@@ -19,6 +21,7 @@ if (hash !== '') {
         elDiv.className = 'col-md-4'
         var elImg = document.createElement('img')
         elImg.src = baseurl + version.snapshot
+        screenshotURLs.push('http://ipfs.io/ipfs/' + version.snapshot)
         elImg.width = '480'
         div.appendChild(elDiv)
         elDiv.appendChild(elImg)
@@ -29,3 +32,16 @@ if (hash !== '') {
   console.log('missing hash')
 }
 
+function createGif () {
+  console.log('GIF')
+  gifshot.createGIF({
+    images: screenshotURLs
+  }, function (obj) {
+    if (!obj.error) {
+      var image = obj.image
+      var animatedImage = document.createElement('img')
+      animatedImage.src = image
+      document.body.appendChild(animatedImage)
+    }
+  })
+}
